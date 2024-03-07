7fc221803bb9145cdf21638878cc09eab2f806f3
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2024-03-07 17:02:02 -0300

    feat: add pages

---
+++ src/data/layout/links.ts
@@ -0,0 +1,20 @@
+import type { Links } from "../../types/link-type";
+
+export const linksData: Links[] = [
+  {
+    title: "companies",
+    path: "/"
+  },
+  {
+    title: "Developers",
+    path: "/developers"
+  },
+  {
+    title: "Jobs",
+    path: "/jobs"
+  },
+  {
+    title: "Blog",
+    path: "/blog"
+  }
+]
